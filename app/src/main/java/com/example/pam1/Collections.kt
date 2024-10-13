f5c2d7a513295d88ce37e7e3b5c5d0dcb8463e76
Mashell0707 <merlsutama@gmail.com>
2024-10-13 20:31:04 +0700

    membuat list mutable dan memcoba menambahkan,menghapus, juga mengubah data dari list mutable

--- app/src/main/java/com/example/pam1/Collections.kt
+++ app/src/main/java/com/example/pam1/Collections.kt
@@ -1,2 +1,29 @@
-package com.example.pam1
 
+
+fun ContohList() {
+    println("=== List ===")
+    // List Read-Only
+
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //List mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    //Menambahkan data ke dalam list mutable
+    shape.add("Circle")
+    println(shape)
+
+    // menghapus data dari list mutable
+    shape.remove( "Triangle")
+    println(shape)
+
+    // mengubah data di dalam list mutable
+    shape[0] ="Oval"
+    println(shape)
+
+    //  list read-only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
