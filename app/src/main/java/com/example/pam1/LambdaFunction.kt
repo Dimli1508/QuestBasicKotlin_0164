3109a311e72b61f48d8afd30660b89ef03630a77
Mashell0707 <merlsutama@gmail.com>
2024-10-13 20:48:28 +0700

    Menggunakan lambda function untuk memisahkan parameter dan body fungsi dengan tanda panah

--- app/src/main/java/com/example/pam1/LambdaFunction.kt
+++ app/src/main/java/com/example/pam1/LambdaFunction.kt
@@ -1,2 +1,17 @@
-package com.example.pam1
+package com.example.pertemuan2
 
+// Lambda Functions
+// Lambda functions adalah fungsi yang tidak memiliki nama
+// Lambda functions biasanya digunakan untuk membuat fungsi yang sederhana
+
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+// Dapat ditulis dalam ekspresi lambda sbb:
+fun main() {
+    uppercaseString("hello")
+    println({string: String -> string.uppercase()} ("hello"))
+    //HELLO
+}
